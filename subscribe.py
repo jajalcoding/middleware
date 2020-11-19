f8d99d12218f07021167519e6431e844c1c5bbcf
jajalcoding <70680913+jajalcoding@users.noreply.github.com>
2020-11-19 21:58:13 +0700

    added tcpsyslog

--- subscribe.py
+++ subscribe.py
@@ -1,9 +1,12 @@
 # pip install httpx[http2]
 # pip install pyaml
+# pip install pysyslogclient
 # r = httpx.post('https://httpbin.org/post', data={'key': 'value'})      https://www.python-httpx.org/quickstart/
+
 import httpx
 import sys
 import yaml
+import pysyslogclient
 
 try:
    namafile = sys.argv[1]
@@ -11,7 +14,15 @@ except:
     print("Need 1 argument, a yaml filename that consist of configuration !")
     exit()
 
-f = open(namafile, "r")
+SERVER = "202.55.91.162"
+PORT = 10514
+
+try:
+    f = open(namafile, "r")
+except:
+    print(namafile+" can not be opened!")
+    exit()
+
 sentdata = yaml.load(f, Loader=yaml.FullLoader)
 f.close()
 print('From YAML:')
@@ -24,8 +35,27 @@ targetserver = sentdata['server']
 sentdata.pop('server')
 
 client = httpx.Client(http2=True)
-response = client.post(targetserver,data=sentdata)
 
+try:
+    response = client.post(targetserver,data=sentdata)
+except:
+    print("Can not reach "+targetserver)
+    exit()
+
+print("Sending to SYSLOG "+SERVER+":"+str(PORT)+"....")
+client = pysyslogclient.SyslogClientRFC5424(SERVER, PORT, proto="TCP")
+client.maxMessageLength = 6000
+
+# need to add error handling here - can not find a way how to error handling this module :(
+
+client.log(response.text,
+	facility=pysyslogclient.FAC_SYSTEM,
+	severity=pysyslogclient.SEV_EMERGENCY,
+	program="Logger",
+	pid=1)
+
+client.close()
+print("-----------")
 print(response.text)
 print("-----------")
 print(response.http_version)
